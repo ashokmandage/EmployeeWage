ee5b457cf3c633e1b06829b4fa8ab97102420b56
AshokA <ashok.mandage22@gmail.com>
2020-08-16 23:02:34 +0530

    adding test case 4 for switch case

---
+++ UsingSwitchCase.java
@@ -0,0 +1,41 @@
+public class EmpWageSwitchCase
+{
+                public static final int IS_FULL_TIME=1;
+                public static final int IS_PART_TIME=2;
+                public static final int EMP_RATE_PER_HOUR=20;
+                 public static final int NUMBER_WORKING_DAYS=20;
+        public static void main(String args[])
+        {
+                EmpWageSwitchCase switchcase=new EmpWageSwitchCase();
+                switchcase.calculate20wage();
+        }
+        public void calculate20wage()
+        {
+                int empHrs;
+                int empWage;
+                int totalEmpWage=0;
+        for(int day=1;day<=NUMBER_WORKING_DAYS;day++)
+        {
+                  int randomCheck=(int)((Math.random()*10)%3);
+
+                 switch(randomCheck)
+                {
+                        case IS_PART_TIME:
+                        empHrs=4;
+                        break;
+
+                        case IS_FULL_TIME:
+                        empHrs=8;
+                        break;
+
+                        default:
+                                empHrs=0;
+                }
+                empWage=empHrs*EMP_RATE_PER_HOUR;
+                totalEmpWage=totalEmpWage+empWage;
+                System.out.println("emloyee wage: "+empWage);
+
+                }
+                System.out.println("totalwage: "+totalEmpWage);
+        }
+}
